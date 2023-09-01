2dd01e9a916ce3ddcf3eb12ec2f055cfdaf9ad09
Vitaliy2773 <129627865+Vitaliy2773@users.noreply.github.com>
2023-09-01 11:12:53 +0000

    Deploying to gh-pages from @ Vitaliy2773/goit-js-hw-09@49d53dca5c1ed762cc473b590f1201dff4e44549 🚀

---
+++ 01-color-switcher.3c9fd30f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");e.addEventListener("click",(()=>{$f00cafd61c2e1654$export$91c4e2f0fa50c4d7=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(()=>{clearInterval($f00cafd61c2e1654$export$91c4e2f0fa50c4d7),e.disabled=!1,t.disabled=!0})),console.log("change");
+//# sourceMappingURL=01-color-switcher.3c9fd30f.js.map
