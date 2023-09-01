ee697b00e3e418f88c66a15cb9b4e062f803f910
Vitaliy2773 <129627865+Vitaliy2773@users.noreply.github.com>
2023-09-01 11:13:27 +0000

    Deploying to gh-pages from @ Vitaliy2773/goit-js-hw-09@946c9c2587918a0ab3071d46a930d2a68596e190 🚀

--- 01-color-switcher.c7c59b61.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={},e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){t.intervalId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),e.disabled=!0,n.disabled=!1}),1e3)})),n.addEventListener("click",(function(){clearInterval(t.intervalId),e.disabled=!1,n.disabled=!0})),console.log("change")}();
-//# sourceMappingURL=01-color-switcher.c7c59b61.js.map
